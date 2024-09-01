84915ab94df208fee2f8a69b1bf2eb2847b550b4
HamodeMuicaa <arauao8881aa@gmail.com>
2024-09-01 00:36:28 +0000

    Update config.py

--- config.py
+++ config.py
@@ -43,11 +43,11 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/HamodeMuicaa/yousef_blyek_muic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/HamodeMuicaa/New_hassen/")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN")
-CH_US = getenv("CH_US", "cecrr")
-CHANNEL_SUDO = getenv("CHANNEL_SUDO", "ceccr")
+CH_US = getenv("CH_US", "SSAAV13")
+CHANNEL_SUDO = getenv("CHANNEL_SUDO", "SSAAV13")
 YAFA_NAME = getenv("YAFA_NAME", "اضغط هنا للاشتراك")
 YAFA_CHANNEL = getenv("YAFA_CHANNEL", "https://t.me/c1xxv")# Fill this variable if your upstream repository is private
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/c1xxv")
